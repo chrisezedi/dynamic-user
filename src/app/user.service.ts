99bd37078e33c57c49a86c0e7041d8f29103e4bb
chrisezedi <91887106+chrisezedi@users.noreply.github.com>
2024-04-27 21:00:14 +0100

    add environments

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserResponsePayload } from './types/user';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private baseUrl = "https://reqres.in/api";
   constructor(private http:HttpClient) { }
 
   getUsers(page:number){
     const params = new HttpParams().set('page', page);
-    return this.http.get<UserResponsePayload>(`${this.baseUrl}/users`,{params});
+    return this.http.get<UserResponsePayload>(`${environment.apiUrl}/users`,{params});
   }
 }
